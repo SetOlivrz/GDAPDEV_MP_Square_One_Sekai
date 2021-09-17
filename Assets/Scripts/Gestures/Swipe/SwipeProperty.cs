e222e552e5fde66021f39b2949f51c550b1f6659
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2021-09-17 16:16:31 +0800

    habol

--- Assets/Scripts/Gestures/Swipe/SwipeProperty.cs
+++ Assets/Scripts/Gestures/Swipe/SwipeProperty.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [System.Serializable]
 public class SwipeProperty 
 {
-    public float minSwipeDistance = 1f;
+    public float minSwipeDistance = 0.875f;
     public float swipeTime = 0.7f;
      
 }
